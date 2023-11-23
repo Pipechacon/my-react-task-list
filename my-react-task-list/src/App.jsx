a6bee8df3c59ad8d72c5e34fbaf1ebddc4e7591f
Pipechacon <felipechaconcardozo@hotmail.com>
2023-11-23 14:56:43 -0500

    Completa funcionalidad y agrega demo

--- my-react-task-list/src/App.jsx
+++ my-react-task-list/src/App.jsx
@@ -4,12 +4,12 @@ import TaskList from './assets/TaskList';
 import TaskForm from './TaskForm'; // Importa el nuevo componente
 
 const initialTasks = [
-  { id: 1, name: 'Tarea 1', completed: false },
-  { id: 2, name: 'Tarea 2', completed: false },
-  { id: 3, name: 'Tarea 3', completed: false },
-  { id: 4, name: 'Tarea 4', completed: false },
-  { id: 5, name: 'Tarea 5', completed: false },
-  { id: 6, name: 'Tarea 6', completed: false },
+  { id: 1, name: 'Tarea 1', description: 'Descripción 1', completed: false },
+  { id: 2, name: 'Tarea 2', description: 'Descripción 2', completed: false },
+  { id: 3, name: 'Tarea 3', description: 'Descripción 3', completed: false },
+  { id: 4, name: 'Tarea 4', description: 'Descripción 4', completed: false },
+  { id: 5, name: 'Tarea 5', description: 'Descripción 5', completed: false },
+  { id: 6, name: 'Tarea 6', description: 'Descripción 6', completed: false },
 ];
 
 function App() {
@@ -22,13 +22,7 @@ function App() {
   }, []);
 
   // Función para agregar tarea
-  const addTask = (name) => {
-    const newTask = {
-      id: tasks.length + 1,
-      name,
-      completed: false,
-    };
-
+  const addTask = (newTask) => {
     const updatedTasks = [...tasks, newTask];
     setTasks(updatedTasks);
 
@@ -68,7 +62,7 @@ function App() {
   return (
     <div style={{ maxWidth: '600px', margin: 'auto', padding: '20px' }}>
       <Header addTask={addTask} />
-      <TaskForm addTask={addTask} /> {/* Agregado el nuevo componente */}
+      <TaskForm addTask={addTask} />
       <TaskList
         tasks={tasks}
         onToggleTask={handleToggleTask}
